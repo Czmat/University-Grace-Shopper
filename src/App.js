45084b96409c4d8aaf313225aeb0b9dcad26a34d
Chaise Matev <aj_fl81@msn.com>
2020-03-17 19:10:27 -0400

    console in app js

--- src/App.js
+++ src/App.js
@@ -6,134 +6,133 @@ import Orders from './Orders';
 import Cart from './Cart';
 import Products from './Products';
 
-const headers = ()=> {
+const headers = () => {
   const token = window.localStorage.getItem('token');
   return {
     headers: {
-      authorization: token
-    }
+      authorization: token,
+    },
   };
 };
 
-const App = ()=> {
-  const [ params, setParams ] = useState(qs.parse(window.location.hash.slice(1)));
-  const [ auth, setAuth ] = useState({});
-  const [ orders, setOrders ] = useState([]);
-  const [ cart, setCart ] = useState({});
-  const [ products, setProducts ] = useState([]);
-  const [ lineItems, setLineItems ] = useState([]);
+const App = () => {
+  console.log('HELLO');
+  const [params, setParams] = useState(qs.parse(window.location.hash.slice(1)));
+  const [auth, setAuth] = useState({});
+  const [orders, setOrders] = useState([]);
+  const [cart, setCart] = useState({});
+  const [products, setProducts] = useState([]);
+  const [lineItems, setLineItems] = useState([]);
 
-  useEffect(()=> {
-    axios.get('/api/products')
-      .then( response => setProducts(response.data));
+  useEffect(() => {
+    axios.get('/api/products').then(response => setProducts(response.data));
   }, []);
 
-  useEffect(()=> {
-    if(auth.id){
+  useEffect(() => {
+    if (auth.id) {
       const token = window.localStorage.getItem('token');
-      axios.get('/api/getLineItems', headers())
-      .then( response => {
+      axios.get('/api/getLineItems', headers()).then(response => {
         setLineItems(response.data);
       });
     }
-  }, [ auth ]);
-  
-  useEffect(()=> {
-    if(auth.id){
-      axios.get('/api/getCart', headers())
-      .then( response => {
+  }, [auth]);
+
+  useEffect(() => {
+    if (auth.id) {
+      axios.get('/api/getCart', headers()).then(response => {
         setCart(response.data);
       });
     }
-  }, [ auth ]);
+  }, [auth]);
 
-  useEffect(()=> {
-    if(auth.id){
-      axios.get('/api/getOrders', headers())
-      .then( response => {
+  useEffect(() => {
+    if (auth.id) {
+      axios.get('/api/getOrders', headers()).then(response => {
         setOrders(response.data);
       });
     }
-  }, [ auth ]);
+  }, [auth]);
 
-  const login = async(credentials)=> {
+  const login = async credentials => {
     const token = (await axios.post('/api/auth', credentials)).data.token;
     window.localStorage.setItem('token', token);
-    exchangeTokenForAuth()
+    exchangeTokenForAuth();
   };
 
-  const exchangeTokenForAuth = async()=> {
+  const exchangeTokenForAuth = async () => {
     const response = await axios.get('/api/auth', headers());
     setAuth(response.data);
-
   };
 
-  const logout = ()=> {
+  const logout = () => {
     window.location.hash = '#';
     setAuth({});
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     exchangeTokenForAuth();
   }, []);
 
-  useEffect(()=> {
-    window.addEventListener('hashchange', ()=> {
+  useEffect(() => {
+    window.addEventListener('hashchange', () => {
       setParams(qs.parse(window.location.hash.slice(1)));
     });
   }, []);
 
-  const createOrder = ()=> {
+  const createOrder = () => {
     const token = window.localStorage.getItem('token');
-    axios.post('/api/createOrder', null , headers())
-    .then( response => {
-      setOrders([response.data, ...orders]);
-      const token = window.localStorage.getItem('token');
-      return axios.get('/api/getCart', headers())
-    })
-    .then( response => {
-      setCart(response.data);
-    });
+    axios
+      .post('/api/createOrder', null, headers())
+      .then(response => {
+        setOrders([response.data, ...orders]);
+        const token = window.localStorage.getItem('token');
+        return axios.get('/api/getCart', headers());
+      })
+      .then(response => {
+        setCart(response.data);
+      });
   };
 
-  const addToCart = (productId)=> {
-    axios.post('/api/addToCart', { productId }, headers())
-    .then( response => {
+  const addToCart = productId => {
+    axios.post('/api/addToCart', { productId }, headers()).then(response => {
       const lineItem = response.data;
-      const found = lineItems.find( _lineItem => _lineItem.id === lineItem.id);
-      if(!found){
-        setLineItems([...lineItems, lineItem ]);
-      }
-      else {
-        const updated = lineItems.map(_lineItem => _lineItem.id === lineItem.id ? lineItem : _lineItem);
+      const found = lineItems.find(_lineItem => _lineItem.id === lineItem.id);
+      if (!found) {
+        setLineItems([...lineItems, lineItem]);
+      } else {
+        const updated = lineItems.map(_lineItem =>
+          _lineItem.id === lineItem.id ? lineItem : _lineItem
+        );
         setLineItems(updated);
       }
     });
   };
 
-  const removeFromCart = (lineItemId)=> {
-    axios.delete(`/api/removeFromCart/${lineItemId}`, headers())
-    .then( () => {
-      setLineItems(lineItems.filter(_lineItem => _lineItem.id !== lineItemId ));
+  const removeFromCart = lineItemId => {
+    axios.delete(`/api/removeFromCart/${lineItemId}`, headers()).then(() => {
+      setLineItems(lineItems.filter(_lineItem => _lineItem.id !== lineItemId));
     });
   };
 
   const { view } = params;
 
-  if(!auth.id){
-    return (
-      <Login login={ login }/>
-    );
-  }
-  else {
+  if (!auth.id) {
+    return <Login login={login} />;
+  } else {
     return (
       <div>
         <h1>Foo, Bar, Bazz.. etc Store</h1>
-        <button onClick={ logout }>Logout { auth.username } </button>
-        <div className='horizontal'>
-          <Products addToCart={ addToCart } products={ products } />
-          <Cart lineItems={ lineItems } removeFromCart={ removeFromCart } cart={ cart } createOrder={ createOrder } products={ products }/>
-          <Orders lineItems={ lineItems } products={ products } orders={ orders }/>
+        <button onClick={logout}>Logout {auth.username} </button>
+        <div className="horizontal">
+          <Products addToCart={addToCart} products={products} />
+          <Cart
+            lineItems={lineItems}
+            removeFromCart={removeFromCart}
+            cart={cart}
+            createOrder={createOrder}
+            products={products}
+          />
+          <Orders lineItems={lineItems} products={products} orders={orders} />
         </div>
       </div>
     );
