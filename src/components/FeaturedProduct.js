f3c53b03c2fcdbbb554c386ed40ce0555330f4f8
cmd08010 <colleendunion@gmail.com>
2020-03-26 19:35:13 -0400

    featured product working

--- src/components/FeaturedProduct.js
+++ src/components/FeaturedProduct.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,37 +8,39 @@ import {
   useParams
 } from "react-router-dom"
 
-const FeaturedProduct = ({ products, getProductDetail }) => {
+const FeaturedProduct = ({ products, getProductDetail, ProductDetail }) => {
   const featuredProduct = products.filter(product => product.rating >= 4)
 
-  console.log(featuredProduct[0], "featured")
-
-  return (
-    <div className="products-box">
-      <h2>Featured Product</h2>
-      {/* <ul>
-        <li className="products">
-          <Link
-            to="/productDetails"
-            onClick={() => getProductDetail(featuredProduct[0].id)}
-          >
-            <img src={featuredProduct[0].image}></img>
-          </Link>
-
-          <span>
+  if (featuredProduct[0]) {
+    return (
+      <div className="products-box">
+        <h2>Featured Product</h2>
+        <ul>
+          <li className="products">
             <Link
               to="/productDetails"
               onClick={() => getProductDetail(featuredProduct[0].id)}
             >
-              {featuredProduct[0].name}
+              <img src={featuredProduct[0].image}></img>
             </Link>
-          </span>
 
-          <span>${Number(featuredProduct[0].price).toFixed(2)}</span>
-        </li>
-      </ul> */}
-    </div>
-  )
+            <span>
+              <Link
+                to="/productDetails"
+                onClick={() => getProductDetail(featuredProduct[0].id)}
+              >
+                {featuredProduct[0].name}
+              </Link>
+            </span>
+
+            <span>${Number(featuredProduct[0].price).toFixed(2)}</span>
+          </li>
+        </ul>
+      </div>
+    )
+  } else {
+    return <div>Select Shop to check out our products!</div>
+  }
 }
 
 export default FeaturedProduct
